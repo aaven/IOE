20d5bbe11fd3786d60f659892379608407adec49
Aaven Jin <aavenj@gmail.com>
2011-07-14 14:51:34 -0400

    move from dropbox to github

---
+++ mobiperf404.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>MobiPerf 404</title>
+<link href="http://mobiperf.com/css/main.css.php.css" rel="stylesheet" type="text/css"> 
+<link rel="shortcut icon" href="/favicon.ico"> 
+<link href="http://mobiperf.com/css/inner.css.php.css" rel="stylesheet" type="text/css">
+</head>
+<body  class="inner">
+<div class="top"> 
+<header class="main wrapper"> 
+<h1 id="logo"> 
+      	<a href="index.html">MobiPerf</a> 
+</h1> 
+</header>
+</div>
+<header id ="title" class="wrapper">
+<h1>404 file not found</h1>
+</header>
+<div class='bottom'>
+<div id="main">
+<div class= "content">
+<div class="body">
+<section>
+<h2><a href="/index">Back to home page</a></h2>
+<h2><a href="/contact">Or contact us</a></h2>
+</section>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
